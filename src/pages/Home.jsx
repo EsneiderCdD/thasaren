e34d359c25ce19db23508ba13dfb00a5e6f2ae7b
EsneiderCdD <ecd.97@hotmail.com>
2026-02-11 06:39:13 -0500

    style(HelloWorld) HelloW Css and fonts

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,10 +4,10 @@ import Navbar from '../components/features/Navbar/Navbar.jsx'
 function Home() {
     return (
         <div className='container'>
-            <h1 className="hola">React Notes + JavaScript</h1>
-            <h2>Pagina web de notas</h2>
-            <p>By Esneider Cadavid</p>
-            <Navbar></Navbar>
+            <Navbar/>
+            <h2>Proyecto Thasaren</h2>
+            <p>Un proyecto que busca llevar al maximo el autoaprenidizaje y la enesanza con IA y autocriterio pro decir algo ps jajaja.</p>
+            
         </div>
     );
 }
